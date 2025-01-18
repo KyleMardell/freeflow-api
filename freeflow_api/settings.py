b1421e1d9f742636d7a174e7391349e5a60bcb6b
Kyle Mardell <kyleslufc@gmail.com>
2025-01-18 19:58:06 +0000

    Add settings and Procfile for Heroku Deployment.

--- freeflow_api/settings.py
+++ freeflow_api/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 if os.path.exists("env.py"):
     import env
@@ -43,6 +44,7 @@ REST_USE_JWT = True
 JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'freeflow-auth'
 JWT_AUTH_REFRESH_COOKIE = 'freeflow-refresh-token'
+JWT_AUTH_SAMESITE = 'None'
 
 REST_AUTH_SERIALIZERS = {
     'USER_DETAILS_SERIALIZER': 'freeflow_api.serializers.CurrentUserSerializer'
@@ -55,12 +57,16 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-kylemardell-freeflowapi-81cqioz7wky.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = [
+    '8000-kylemardell-freeflowapi-81cqioz7wky.ws.codeinstitute-ide.net',
+    'https://freeflow-api-5d3396ab0611.herokuapp.com/'
+    ]
 
 CSRF_TRUSTED_ORIGINS = [
     'https://8000-kylemardell-freeflowapi-81cqioz7wky.ws.codeinstitute-ide.net',
+    'https://freeflow-api-5d3396ab0611.herokuapp.com/'
 ]
 
 
@@ -84,6 +90,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'dj_rest_auth.registration',
+    'corsheaders',
 
     'profiles',
     'projects',
@@ -94,6 +101,7 @@ INSTALLED_APPS = [
 SITE_ID = 1
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -102,6 +110,17 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+if 'CLIENT_ORIGIN' in os.environ:
+    CORS_ALLOWED_ORIGINS = [
+        os.environ.get('CLIENT_ORIGIN')
+    ]
+else:
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        r"^https:\/\/.*\.codeinstitute-ide\.net$",
+    ]
+
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'freeflow_api.urls'
 
@@ -127,12 +146,17 @@ WSGI_APPLICATION = 'freeflow_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DEV' in os.environ:
+     DATABASES = {
+         'default': {
+             'ENGINE': 'django.db.backends.sqlite3',
+             'NAME': BASE_DIR / 'db.sqlite3',
+         }
+     }
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
-}
 
 
 # Password validation
