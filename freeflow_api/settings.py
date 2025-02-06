f49d712290e64b10c6b8a3e0676f5ac014ed0148
Kyle Mardell <kyleslufc@gmail.com>
2025-02-06 18:39:41 +0000

    Set debug back to DEV, set to false in heroku.

--- freeflow_api/settings.py
+++ freeflow_api/settings.py
@@ -59,8 +59,7 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEV' in os.environ
-DEBUG = True
+DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-kylemardell-freeflowapi-81cqioz7wky.ws.codeinstitute-ide.net',
